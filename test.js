87b67d1a23e8e1541458e4977c71a57fd9431c31
Evan Hahn <me@evanhahn.com>
2026-07-05 18:31:01 -0500

    Use Node's test runner

--- test.js
+++ test.js
@@ -1,24 +1,26 @@
 import assert from "node:assert/strict";
+import test from "node:test";
 import { deepProxy } from "./deep-proxy.js";
 
-// Deep property assignment
-const obj = { foo: "boo" };
-const proxy = deepProxy(obj);
+test("deep property assignment", () => {
+  const obj = { foo: "boo" };
 
-assert.deepEqual(obj, { foo: "boo" });
+  const proxy = deepProxy(obj);
+  assert.deepEqual(obj, { foo: "boo" });
 
-proxy.very.deeply.nested.property = "wow";
-
-assert.deepEqual(obj, {
-  foo: "boo",
-  very: {
-    deeply: {
-      nested: {
-        property: "wow",
+  proxy.very.deeply.nested.property = "wow";
+  assert.deepEqual(obj, {
+    foo: "boo",
+    very: {
+      deeply: {
+        nested: {
+          property: "wow",
+        },
       },
     },
-  },
+  });
 });
 
-// Deep property access
-assert.deepEqual(deepProxy({}).deeply.nested.property, {});
+test("deep property access", () => {
+  assert.deepEqual(deepProxy({}).deeply.nested.property, {});
+});
